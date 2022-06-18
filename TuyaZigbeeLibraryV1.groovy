9dc71d880cd2c3445df7ac11f9ce7d1a198ae0ac
kkossev <kossevkrassimir@gmail.com>
2022-06-18 23:06:08 +0300

    Create TuyaZigbeeLibraryV1.groovy

---
+++ TuyaZigbeeLibraryV1.groovy
@@ -0,0 +1,38 @@
+library (
+        name: "TuyaZigbeeLibraryV1",
+        namespace: "kkossev",
+        author: "Krassimir Kossev",
+        description: "Tuya",
+        category: "zigbee",
+        base: "driver",
+        documentationLink: ""
+)
+
+/**
+ *  Tuya Zigbee Library
+ *
+ *  This is a HE library to be used with Tuya Zigbee devices drivers.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ * 
+ * 
+ * ver. 1.0.0 2022-06-18 kkossev  - first version
+ *
+*/
+
+//
+// usage: #include kkossev.TuyaZigbeeLibrary
+//  #include's parameters (if one can call them that) is namespace, followed by a dot, followed by name. Names and namespaces can include letters (upper/lower case), numbers, dots, dashes, and underscores.
+//  Required attributes are namespace, name, description, and author.
+// All apps/drivers using include are recompiled when include gets a change. Each driver uses the same version of a library. You put common code in the library, and driver specific code in the driver itself.
+//
+
+
+void method1() {}
